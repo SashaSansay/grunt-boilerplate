d0bb5ac742d86eec9f8f3827b9966ac56970aabc
alexadergorshkov <mc_nine@bk.ru>
2018-08-28 10:55:32 +0400

    flow type doc

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,5 @@
+// Set your own font-size using the fontRatio variable.
+// When using fontRatio, increase the value to make the font smaller (and vice verse).
 $('html').flowtype({
     fontRatio : 30,
     minFont : 10,
